8c8756554d8793eff8bbc69a94195430ad82c806
Valemia <youanyinke@gmail.com>
2023-03-27 15:26:12 -0400

    Assignment Main 1 done

---
+++ src/main/java/array/Main1.java
@@ -0,0 +1,13 @@
+package array;
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        int[] myIntArray = {1, 2, 3};
+
+        for (int i = 0; i < myIntArray.length; i++) {
+            System.out.println(myIntArray[i]);
+
+        }
+    }
+}
