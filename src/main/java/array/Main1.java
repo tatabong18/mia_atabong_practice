9bee444634211bd52b43e179e0ce8dba66dbb921
Valemia <youanyinke@gmail.com>
2023-03-27 17:15:45 -0400

    Assignment Main2 done

--- src/main/java/array/Main1.java
+++ src/main/java/array/Main1.java
@@ -1,14 +1,27 @@
 package array;
 
+import java.util.Arrays;
+
 public class Main1 {
     public static void main(String[] args) {
 
         int[] myIntArray = {1, 2, 3};
         //making a change
 
-        for (int i = 0; i < myIntArray.length; i++) {
-            System.out.println(myIntArray[i]);
 
-        }
-    }
-}
+       // for (int i = 0; i < myIntArray.length; i++) {
+        System.out.println(Arrays.toString(myIntArray));
+         //   System.out.println(myIntArray[i]);
+
+
+          //      int[] arr = new int[3];
+          //      arr[0] = 1;
+          //      arr[1] = 2;
+          //      arr[2] = 3;
+           //     for (int i = 0; i < arr.length; i++) {
+            //        System.out.println("Array element " + i + " is " + arr[i]);
+                }
+            }
+
+
+
---
+++ src/main/java/array/Main2.java
@@ -0,0 +1,11 @@
+package array;
+
+public class Main2 {
+        public static void main(String[] args) {
+            int[] arr = {13, 5, 7, 68, 2};
+            int middleIndex = arr.length / 2;
+            int middleElement = arr[middleIndex];
+            System.out.println("Middle element: " + middleElement);
+        }
+    }
+
