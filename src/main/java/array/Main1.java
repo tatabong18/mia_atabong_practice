3fd4b5156f54528a97b8a8a55f4c252d2c99a4f9
Valemia <youanyinke@gmail.com>
2023-03-27 15:29:05 -0400

    Assignment Main 1 done with minor change

--- src/main/java/array/Main1.java
+++ src/main/java/array/Main1.java
@@ -4,6 +4,7 @@ public class Main1 {
     public static void main(String[] args) {
 
         int[] myIntArray = {1, 2, 3};
+        //making a change
 
         for (int i = 0; i < myIntArray.length; i++) {
             System.out.println(myIntArray[i]);
