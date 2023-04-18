d71d1e9963486e09e73de7928ffec8b34e297cf8
Valemia <youanyinke@gmail.com>
2023-04-18 18:07:32 -0400

    SBA exercises part #2 create an Item Class

--- src/main/java/sba_exercise/Item.java
+++ src/main/java/sba_exercise/Item.java
@@ -6,21 +6,18 @@ public class Item {
     private Integer quantity;
     private Integer availableQuantity;
 
-    // Default constructor
     public Item() {
         this.quantity = 1;
     }
 
-    // Constructor with 4 arguments
     public Item(String itemName, String itemDescription, Double price, Integer availableQuantity) {
-        this(); // Calling default constructor
+        this(); // call the default constructor
         this.itemName = itemName;
         this.itemDescription = itemDescription;
         this.price = price;
         this.availableQuantity = availableQuantity;
     }
 
-    // Getters and Setters
     public String getItemName() {
         return itemName;
     }
@@ -59,20 +56,5 @@ public class Item {
 
     public void setAvailableQuantity(Integer availableQuantity) {
         this.availableQuantity = availableQuantity;
-    }
-
-    // Main method for testing
-    public static void main(String[] args) {
-        // Creating an instance of Item with test data
-        Item item = new Item("noodle", "really good", 20.00, 20);
-
-        // Displaying the item details
-        System.out.println("----------- Testing the constructor of Item class -----");
-        System.out.printf("%-20s%-20s%-10.2f%-10d%-10d%n",
-                item.getItemName(),
-                item.getItemDescription(),
-                item.getPrice(),
-                item.getQuantity(),
-                item.getAvailableQuantity());
     }
 }
--- src/main/java/sba_exercise/Main.java
+++ src/main/java/sba_exercise/Main.java
@@ -1,35 +1,72 @@
 package sba_exercise;
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("----------- Testing reading sample.txt -----");
+//        System.out.printf("%-20s %-20s %-10s %-10s\n", "Name", "Description", "Price", "Quantity Available");
+//        try {
+//            File file = new File("sample.txt");
+//            Scanner scanner = new Scanner(file);
+//            while (scanner.hasNextLine()) {
+//                String line = scanner.nextLine();
+//                String[] items = line.split("\\s{2}");
+//                String name = items[0];
+//                String description = items[1];
+//                double price = Double.parseDouble(items[2]);
+//                int quantity = Integer.parseInt(items[3]);
+//                System.out.printf("%-20s %-20s %-10.2f %-10d\n", name, description, price, quantity);
+//            }
+//            scanner.close();
+//            // default exception handler
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.Formatter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("----------- Testing reading sample.txt -----");
-        System.out.printf("%-20s%-20s%-10s%-20s%n",
+        System.out.printf("%-20s %-20s %-10s %-10s\n",
                 "Name", "Description", "Price", "Quantity Available");
         try {
             File file = new File("sample.txt");
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                String[] fields = line.split("  ");
-                String itemName = fields[0];
-                String itemDescription = fields[1];
-                double itemPrice = Double.parseDouble(fields[2]);
-                int availableQuantity = Integer.parseInt(fields[3]);
-                System.out.printf("%-20s%-20s%-10.2f%-20d%n",
-                        itemName, itemDescription, itemPrice,
-                        availableQuantity);
+                String[] items = line.split("\\s{2}");
+                String name = items[0];
+                String description = items[1];
+                double price = Double.parseDouble(items[2]);
+                int quantity = Integer.parseInt(items[3]);
+                System.out.printf("%-20s %-20s %-10.2f %-10d\n",
+                        name, description, price, quantity);
             }
             scanner.close();
+            // default exception handler
         } catch (FileNotFoundException e) {
-            // Propagate FileNotFoundException
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+
+        System.out.println("\n----------- Testing the constructor " +
+                "of Item class -----");
+        Item item = new Item("Jollof Rice",
+                "Very yummy",
+                16.00,
+                12);
+        System.out.printf("%-20s %-20s %-10.2f %-10d %-10d\n",
+                item.getItemName(),
+                item.getItemDescription(),
+                item.getPrice(),
+                item.getQuantity(),
+                item.getAvailableQuantity());
     }
 }
